8bd85739dd0e4323a75ec581b1aa4d8c74a8eb4d
Shawshank980924 <Akatsuki980924@163.com>
2021-12-10 21:36:36 +0800

    No172 and No504 finished

---
+++ src/leecode/math/N0504/Solution.java
@@ -0,0 +1,5 @@
+package leecode.math.N0504;/**
+ * @author 小羊Shaun
+ * @version 1.0
+ */public class Solution {
+}
---
+++ src/leecode/math/No172/Solution.java
@@ -0,0 +1,5 @@
+package leecode.math.No172;/**
+ * @author 小羊Shaun
+ * @version 1.0
+ */public class Solution {
+}
