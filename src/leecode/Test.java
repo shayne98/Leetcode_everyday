a74ec7a38c1d9c9ccfdce8417427161566954889
Shawshank980924 <Akatsuki980924@163.com>
2021-12-17 13:57:00 +0800

    No 650. 2 Keys Keyboard reviewed

--- src/leecode/Test.java
+++ src/leecode/Test.java
@@ -10,23 +10,20 @@ import java.util.*;
 @SuppressWarnings("all")
 
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int len1 = text1.length();
-        int len2 = text2.length();
-        char[] char1 = text1.toCharArray();
-        char[] char2 = text2.toCharArray();
-        //dp[i][j]表示到第i和j位置text1 text2最长公共子序列的长度
-        int[][] dp = new int[len1+1][len2+1];
-        for(int i=1;i<len1;i++){
-            for(int j=1;j<len2;j++){
-                if(char1[i-1]==char2[j-1]){
-                    dp[i][j] = dp[i-1][j-1]+1;
-                }
-                else{
-                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+    public static void main(String[] args) {
+        new Solution().minSteps(6);
+    }
+    public int minSteps(int n) {
+        int[] dp = new int[n+1];
+        for(int i=2;i<=n;i++){
+            dp[i] = i;
+            for(int j=2;j*j<=i;j++){
+                if(i%j==0){
+                    int num = i/j;
+                    dp[i] = dp[num]+j;
                 }
             }
         }
-        return dp[len1][len2];
+        return dp[n];
     }
 }
